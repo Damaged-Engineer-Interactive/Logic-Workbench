dbeec2670e5aa0b23981d3136f7e8921c076cc3a
Logic Mindful <master51.main@gmail.com>
2025-05-26 15:46:42 +0200

    UI Revamp Part 2

--- scripts/credits.gd
+++ scripts/credits.gd
@@ -9,5 +9,13 @@ func _ajax_discord_pressed() -> void:
 	OS.shell_open("https://discord.gg/gNtqTZXr4u")
 
 
+func _rascal_github_pressed() -> void:
+	OS.shell_open("https://github.com/RascalFoxfire")
+
+
+func _rascal_bluesky_pressed() -> void:
+	OS.shell_open("https://bsky.app/profile/rascalfoxfire.bsky.social")
+
+
 func _main_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
